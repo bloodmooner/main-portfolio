6cf203165df81ddc837007a4e65ad1a537b68468
Sergey Goncharuk <fleshed914@gmail.com>
2021-09-29 09:44:23 +0300

    feat: add modal background to menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,14 @@
 const burgerElem = document.querySelector('.burger');
 const burgerMenuClose = document.querySelector('.burger-menu__close');
 const burgerMenu = document.querySelector('.burger-menu');
+const modalBG = document.querySelector('.modal-bg');
 
 burgerElem.addEventListener('click', () => {
   burgerMenu.classList.remove('burger-menu_closed');
+  modalBG.style.display = 'block';
 });
 
 burgerMenuClose.addEventListener('click', () => {
   burgerMenu.classList.add('burger-menu_closed');
+  modalBG.style.display = 'none';
 });
