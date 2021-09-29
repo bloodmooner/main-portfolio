3ab7b5d5fd19f412592d7944254eb0e15ab9397d
Sergey Goncharuk <fleshed914@gmail.com>
2021-09-29 09:09:06 +0300

    feat: add script to show/hide .menu, border to .menu elems

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const burgerElem = document.querySelector('.burger');
+const burgerMenuClose = document.querySelector('.burger-menu__close');
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerElem.addEventListener('click', () => {
+  burgerMenu.classList.remove('burger-menu_closed');
+});
+
+burgerMenuClose.addEventListener('click', () => {
+  burgerMenu.classList.add('burger-menu_closed');
+});
