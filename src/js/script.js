97411903a9912f5888ce8b50111aa598bdb9a8b3
Sergey Goncharuk <fleshed914@gmail.com>
2021-10-09 08:40:12 +0300

    feat: add select category, animation to test page.html

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ const burgerMenuClose = document.querySelector('.burger-menu__close');
 const burgerMenu = document.querySelector('.burger-menu');
 const modalBG = document.querySelector('.modal-bg');
 
-burgerElem.addEventListener('click', () => {
+/* burgerElem.addEventListener('click', () => {
   burgerMenu.classList.remove('burger-menu_closed');
   modalBG.style.display = 'block';
 });
@@ -14,3 +14,17 @@ burgerMenuClose.addEventListener('click', () => {
   burgerMenu.classList.add('burger-menu_closed');
   modalBG.style.display = 'none';
 });
+ */
+
+const selectCategory = document.querySelector('.controls__item_category');
+const selectTriangle = selectCategory.querySelectorAll('.controls__triangle-line');
+const selectContainer = selectCategory.querySelector('.controls__select-container');
+
+
+selectCategory.addEventListener('click', () => {
+  selectCategory.classList.toggle('controls__item_active');
+  selectTriangle.forEach((triangle) => {
+    triangle.classList.toggle('controls__triangle-line_down');
+  });
+  selectContainer.classList.toggle('controls__select-container_active')
+})
